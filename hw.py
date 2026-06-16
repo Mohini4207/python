79a0fc76e8a19ef8c9813a685971e5e043c24899
Mohini4207 <mohinisuthar2007@gmail.com>
2026-06-16 15:57:51 +0000

    hw

---
+++ hw.py
@@ -0,0 +1,9 @@
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+mylist.insert(2,"mango")
+print(mylist)
+mylist[1] = "blueberry"
+print(mylist)
+mylist.remove("banana")
+print(mylist)
+del mylist
---
+++ m2.py
@@ -0,0 +1,28 @@
+a=20
+b=10
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print(add)
+print(sub)
+print(mul)
+print(div)
+
+x = 35e3
+print(type(x))
+a=4207
+print(type(a))
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = float(1)
+print(x)
+
+y = str(2)
+print(y)
